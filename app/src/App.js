396b68e28c462f37a8cb85a417ec1174de7845dc
Andrew Park <andrewpark2396@gmail.com>
2024-03-24 04:11:28 -0700

    Delete added, code refactored, with basic CSS

--- app/src/App.js
+++ app/src/App.js
@@ -6,31 +6,14 @@ function App() {
   const data = ['A', 'B', 'C'];
 
   const [item, setItem] = useState('');
-  const [grocery, setGrocery] = useState([]);
-
-  function handleGroceryItems() {
-    console.log(item);
-    console.log(typeof item);
-    if (grocery.includes(item) || item === '') {
-      setItem('');
-    } else {
-    const updatedList = grocery.concat(item);
-    setGrocery(updatedList);
-    console.log(updatedList);
-    setItem('');
-    }
-  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <select onChange={(e) => setItem(e.target.value)} >
-          {data.map((e) => <option>{e}</option>)}
+        <select className="select-box" onChange={(e) => setItem(e.target.value)} >
+          {data.map((e) => <option className="option-item">{e}</option>)}
         </select>
-        <GroceryList data={grocery}/>
-        <button onClick={handleGroceryItems}>
-          Add Items
-        </button>
+        <GroceryList data={item}/>
        
       </header>
     </div>
--- app/src/components/GroceryList.js
+++ app/src/components/GroceryList.js
@@ -1,16 +1,49 @@
-import React, {memo} from "react";
+import React, {memo, useState} from "react";
 
 const GroceryList = memo(function GroceryList({data}) {
-    return (
-        <ul>
-            {data.map(item => {
-                return <li>
-                    {item}
-                </li>
-            } )}
-        </ul>
-    )
 
+    const [grocery, setGrocery] = useState([]);
+
+    console.log(data);
+    console.log(typeof data);
+
+    function handleAddGrocery() {
+        if (grocery.includes(data) || data === '') {
+            return;
+
+        } else {
+            const updatedList = grocery.concat(data);
+            setGrocery(updatedList);
+            console.log(updatedList);
+        }
+    }
+
+    function handleRemoveGrocery(index) {
+        const updatedList = grocery.filter((_, i) => i !== index);
+        setGrocery(updatedList);
+    }
+
+    console.log(grocery);
+    
+    return (
+        <div className="grocery-list">
+            <div>
+                {grocery && grocery.length > 0 && (
+                <ul>
+                    {grocery.map((item,index) => {
+                        return <li key={index}>
+                            <span>{item}</span>
+                            <button className="delete-button" onClick={() => handleRemoveGrocery(index)}>Delete item </button>
+                        </li>
+                    } )}
+                </ul>
+                )}
+                <button className="add-button" onClick={handleAddGrocery}>
+                    Add Items
+                </button>
+            </div>
+        </div>
+    )
 });
 
 export default GroceryList;
